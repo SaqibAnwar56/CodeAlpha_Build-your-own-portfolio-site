e887a528a3998c21cd81716c4907c4b6cd6aa204
Saqib Abro <abrosaqi78@gmail.com>
2025-06-14 12:07:09 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+// Highlight current section in nav while scrolling
+window.addEventListener("scroll", () => {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll("nav a");
+
+  let current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (scrollY >= sectionTop - 60) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach((link) => {
+    link.classList.remove("active");
+    if (link.getAttribute("href") === `#${current}`) {
+      link.classList.add("active");
+    }
+  });
+});
